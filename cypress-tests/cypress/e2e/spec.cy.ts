8f064d74d2acbf6d90c22bb54691516d6581b47a
Andy Rivero Martin <riveroandy02@gmail.com>
2025-08-07 10:44:22 -0400

    feat: fail test

--- cypress-tests/cypress/e2e/spec.cy.ts
+++ cypress-tests/cypress/e2e/spec.cy.ts
@@ -1,6 +1,9 @@
 describe("Test", () => {
   it("test", () => {
-    cy.visit('/')
-    cy.get(".text-3xl").should("exist").and("have.text", "Welcome");
+    cy.visit("/");
+    cy.get(".text-3xl").should("exist");
+    cy.get(".my-4 > .text-pink-600").should("have.text", "35 productos encontrados");
+    cy.get('select').first().select('Men')
+    cy.get('.my-4 > .text-pink-600').should("have.text", "13 productos encontrados");
   });
 });
